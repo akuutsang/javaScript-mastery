bec216ad1fa0b7bb5ae6407575a23e5f9f6db9d3
akutsang <akuutsang@gmail.com>
2025-05-22 22:34:15 +0100

    mastering conditionals in js

---
+++ conditional/script.js
@@ -0,0 +1,9 @@
+let age = 17;
+let password = "BABYLEE";
+let isVip = true;
+
+if (age > 18 && (password === "BABYLEE" || isVip)) {
+  console.log("Come on in!!! Its party time!!!");
+} else {
+  console.log("Please try another place!!!!!!!!");
+}
