0321247404ecf7359b2a5758741f945500d68bf9
akutsang <akuutsang@gmail.com>
2025-05-27 12:20:02 +0100

    illustrating the diff between dot notation and bracket notation

--- conditional/script.js
+++ conditional/script.js
@@ -1,9 +1,22 @@
-let age = 17;
-let password = "BABYLEE";
-let isVip = true;
+// let age = 17;
+// let password = "BABYLEE";
+// let isVip = true;
 
-if (age > 18 && (password === "BABYLEE" || isVip)) {
-  console.log("Come on in!!! Its party time!!!");
-} else {
-  console.log("Please try another place!!!!!!!!");
-}
+// if (age > 18 && (password === "BABYLEE" || isVip)) {
+//   console.log("Come on in!!! Its party time!!!");
+// } else {
+//   console.log("Please try another place!!!!!!!!");
+// }
+
+let user = {
+  name: "Limama",
+  age: 2,
+  birthday: "29th July 2023",
+};
+
+let currentUser = user["name"];
+let birthyear = "birthday";
+
+console.log(user);
+console.log(user[birthyear]);
+console.log(user.birthyear);
