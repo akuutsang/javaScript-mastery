846afb22b1e14d5c3ad33316ff94945933c909c2
akutsang <akuutsang@gmail.com>
2025-06-11 13:52:04 +0100

    Object constructors with class contructor

--- jsBasics/script.js
+++ jsBasics/script.js
@@ -16,3 +16,23 @@ function Book(title, author, pages, hasRead) {
 const theHobbit = new Book("The Hobbits", "J.R.R Tolkien", "295", true);
 
 console.log(theHobbit.bookDetails());
+
+// OBJECT CONTRUCTORS; with class constructor
+class Book2 {
+  constructor(title, author, pages, isRead) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.isRead = isRead; // true or false
+  }
+
+  info() {
+    const readStatus = this.isRead ? "already read" : "not read yet";
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${readStatus}`;
+  }
+}
+
+const theHobbit2 = new Book2("The Hobbit", "J.R.R. Tolkien", 295, false);
+
+console.log(theHobbit2.info());
+// Output: "The Hobbit by J.R.R. Tolkien, 295 pages, not read yet"
